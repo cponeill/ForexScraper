5ebf94e13be42edbc94d77b296c21defbb72b1ec
Ubuntu <caseyoneill78@hotmail.com>
2017-10-16 14:12:39 +0000

    adding more commented code.

--- forex/forex/spiders/currency_spider.py
+++ forex/forex/spiders/currency_spider.py
@@ -16,7 +16,10 @@ class CurrencySpider(scrapy.Spider):
     start_urls = ['http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html']
 
     def parse(self, response):
+        # set overall xpath value to markets
         markets = response.xpath('//tbody/tr')
+
+        # loop through markets variable, set each path for symbol and price, and output json.
         for market in markets:
             symbol = market.xpath('td[@class="currency"]/a/text()').extract_first()
             price = market.xpath('td[@class="spot number"]/a/span/text()').extract_first()
